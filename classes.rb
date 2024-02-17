f6c66dca3bc84b5b6e8f44885a91bcae1a4eb6d7
JuniorConde <140565495+JUNIORCONDE@users.noreply.github.com>
2024-02-17 10:52:21 -0300

    UP2

--- classes.rb
+++ classes.rb
@@ -7,8 +7,10 @@ class Animal
 # de uma classe, precisamos programar a classe para que deixa fornecer esses valores.
 # usamos metodos de acesso getters setters
 
-attr_reader :nome
-attr_writer :nome
+#attr_reader :nome
+#attr_writer :nome
+attr_accessor :nome 
+attr_accessor :especie
 
 
 # precisamos criar um metodo de construção, toda vez que criamos um objeto dentro de uma classe
@@ -25,9 +27,17 @@ attr_writer :nome
     end
 
 end
+# dessa forma estamos colocando os nomes, dados, informação de forma automática, sem inserção do usuário
+#pet = Animal.new("Totó", "Cachorro")
+#puts pet.nome
+#puts pet.especie
 
-pet = Animal.new()
+# dessa forma o usuário precisa fornecer informção, entrada do teclado.
+pet = Animal.new("...", "...") # aqui precisamos deixar qual valor, pois ele vai sofrer alteração, e não deixa vai da erro.
+puts "Qual nome do seu Animal de estimação?"
+    pet.nome = gets.chomp()
+puts "Qual a raça do seu bichinho?"
+    pet.especie = gets.chomp()
 
-
-
-
+puts pet.nome
+puts pet.especie
