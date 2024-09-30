1028fa55c01747e11bc0b3206e6ef9538354a21d
JUNIORCONDE <rubenscondejr@uol.com.br>
2024-09-30 05:46:58 -0300

    feat(estrutura de condicão)

---
+++ condicionais.rb
@@ -0,0 +1,13 @@
+puts "Digite um numero:"
+n1 = gets.chomp.to_i
+
+if n1 >= 10 then
+	puts "O Valor é igual ou maior que 10"
+
+elsif n1 < 5
+	puts " O valor é igual ou menor que 5"
+
+else n1 > 10
+	puts "O valor é menor 10 e maior que 5"
+
+end
