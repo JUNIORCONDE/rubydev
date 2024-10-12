d9f9a3a75eb24bf3ec9a268adc67c30714447d55
JUNIORCONDE <rubenscondejr@uol.com.br>
2024-10-12 10:17:47 -0300

    (anotações, estudos)

--- nome.rb
+++ nome.rb
@@ -7,3 +7,24 @@ anoat = gets.chomp.to_i
 idade = anonasc - anoat
 
 puts "Sua idade atual é #{idade}"
+
+
+
+=begin
+puts "QUAL É SEU NOME?"
+nome = gets.chomp
+puts "QUAL É SUA IDADE?"
+idade = gets.chomp.to_i
+
+puts "HELLO #{nome}"
+puts "#{nome} tem #{idade} anos"
+
+=end
+
+#puts "QUAL É SEU NOME?"
+#nome = gets.chomp
+#puts "QUAL É SUA IDADE?"
+#idade = gets.chomp.to_i
+
+#puts "HELLO #{nome}"
+#puts "#{nome} tem #{idade} anos"
